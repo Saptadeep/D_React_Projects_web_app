64594bbbe158c6ec392dc03f5882f959e351722b
Saptadeep <Saptadeep>
2017-09-14 06:10:33 +0530

    Routine commit

--- src/App.js
+++ src/App.js
@@ -9,25 +9,49 @@ export default class App extends Component {
       <div>
       {
       //This is a comment
-    }
-      <TheCalledComponent placeHolder = "123" />
-      <TheCalledComponent placeHolder = "456" />
+      }
+        <NameForm />
       </div>
     )
   }
 }
 
-class TheCalledComponent extends Component{
-  constructor(props){
-    super(props)
-    alert(this.props.placeHolder)
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    {console.log(this.state)}
   }
 
-  render(){
-    return(
-      <h1>
-      {this.props.placeHolder}
-      </h1>
-    )
-}
+  handleChange(event) {
+    alert('event.target.value: ' + event.target.value);    
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+      {console.log(this.state.value)}
+    return (
+
+      <div>
+
+      <form onSubmit={this.handleSubmit}>
+
+      {console.log(this)}
+        <label>
+          Name:
+          <input type="text"  />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
+    );
+  }
 }
---
+++ src/App_Demo3.js
@@ -0,0 +1,33 @@
+import React,  { Component }  from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SayHello from './common/reusableComponents/helloWorld';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      {
+      //This is a comment
+      }
+      <TheCalledComponent placeHolder = "123" />
+      <TheCalledComponent placeHolder = "456" />
+      </div>
+    )
+  }
+}
+
+class TheCalledComponent extends Component{
+  constructor(props){
+    super(props)
+    alert(this.props.placeHolder)
+  }
+
+  render(){
+    return(
+      <h1 style={{ color: `tomato` }}>
+      {this.props.placeHolder}
+      </h1>
+    )
+}
+}
